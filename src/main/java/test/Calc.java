d5b11bdf5d1a50edb48331ddd4b2ca538d26d0f8
koczkaD <winifred0318@gmail.com>
2017-03-06 14:11:31 +0100

    Calc functions added

---
+++ src/main/java/test/Calc.java
@@ -0,0 +1,22 @@
+package test;
+
+/**
+ * Created by winifred on 2017.03.06..
+ */
+public class Calc {
+
+    public double sum(double number1, double number2){
+        return number1 + number2;
+    }
+
+    public double sub(double number1, double number2){
+        return number1-number2;
+    }
+
+    public double mult(double num1, double num2){
+        return num1*num2;
+    }
+    public double div(double num1, double num2){
+        return num1/num2;
+    }
+}
